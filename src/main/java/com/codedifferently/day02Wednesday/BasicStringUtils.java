c1c6a40a2387b6489f49d4261c47071c5f26cc2e
Matt Werth <matthewwerth@gmail.com>
2020-11-04 11:18:46 -0500

    Finished ReverseWords 1

--- src/main/java/com/codedifferently/day02Wednesday/BasicStringUtils.java
+++ src/main/java/com/codedifferently/day02Wednesday/BasicStringUtils.java
@@ -1,12 +1,19 @@
 package com.codedifferently.day02Wednesday;
 
+import java.util.Arrays;
+
 public class BasicStringUtils {
     /**
      * @param str string input from client
      * @return string with identical content, and the first character capitalized
      */
     public static String camelCase(String str) {
-        return null;
+        String finalWord = "";
+        String[] split = str.split(" ");
+        for(int i = 0; i < split.length; i ++) {
+            finalWord += Character.toUpperCase(split[i].charAt(0)) + split[i].substring(1) + " ";
+        }
+        return finalWord.trim();
     }
 
     /**
@@ -14,14 +21,30 @@ public class BasicStringUtils {
      * @return string with identical contents, in the reverse order
      */
     public static String reverse(String str) {
-        return null;
+        String[] split = str.split("");
+
+        for(int i = 0; i < split.length/2; i++) {
+            String temp = split[i];
+            split[i] = split[split.length-i -1];
+            split[split.length -i -1] = temp;
+        }
+        return String.join("", split);
+
+//        String rstring = "";
+//        String[] split = str.split("");
+//
+//        for(int i = split.length -1; i >= 0; i--) {
+//            rstring += split[i];
+//        }
+//        return rstring;
     }
 
     /**
      * @param str string input from client
      * @return string with identical contents, with each word individually in reverse order
      */
-    public static String reverseWords(String str) {
+    public static String reverseWords(String str) { //reverse words in current position.
+
         return null;
     }
 
