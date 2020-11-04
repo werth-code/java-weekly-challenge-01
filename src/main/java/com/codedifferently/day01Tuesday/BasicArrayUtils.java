65a7cf3ea36be895f6dcd7ee64558956347acd67
Matt Werth <matthewwerth@gmail.com>
2020-11-04 08:20:41 -0500

    Finished Array Utils

--- src/main/java/com/codedifferently/day01Tuesday/BasicArrayUtils.java
+++ src/main/java/com/codedifferently/day01Tuesday/BasicArrayUtils.java
@@ -1,12 +1,16 @@
 package com.codedifferently.day01Tuesday;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class BasicArrayUtils {
     /**
      * @param stringArray an array of String objects
      * @return the first element in the array
      */
     public static String getFirstElement(String[] stringArray) {
-        return null;
+        return stringArray[0];
     }
 
     /**
@@ -14,15 +18,21 @@ public class BasicArrayUtils {
      * @return the second element in the array
      */
     public static String getSecondElement(String[] stringArray) {
-        return null;
+        return stringArray[1];
     }
 
     /**
      * @param stringArray an array of String objects
      * @return stringArray with the elements in reverse order
      */
+
     public static String[] reverse(String[] stringArray) {
-        return null;
+        for(int i = 0; i < stringArray.length/2; i++) {
+            String temp = stringArray[i];
+            stringArray[i] = stringArray[stringArray.length - i -1];
+            stringArray[stringArray.length -i - 1] = temp;
+        }
+        return stringArray;
     }
 
     /**
@@ -30,6 +40,10 @@ public class BasicArrayUtils {
      * @return String made up of the first character in each element of stringArray
      */
     public static String getFirstLetterOfEachElement(String[] stringArray) {
-        return null;
+        String chars = "";
+        for(String ele : stringArray) {
+            chars += ele.charAt(0);
+        }
+        return chars;
     }
 }
