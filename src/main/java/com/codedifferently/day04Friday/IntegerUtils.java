f3b61cb7eeb880ba8e582291aace222debafec44
Matt Werth <matthewwerth@gmail.com>
2020-11-05 09:24:45 -0500

    Finished Friday IntegerUtils

--- src/main/java/com/codedifferently/day04Friday/IntegerUtils.java
+++ src/main/java/com/codedifferently/day04Friday/IntegerUtils.java
@@ -1,12 +1,18 @@
 package com.codedifferently.day04Friday;
 
+import static java.lang.Integer.parseInt;
+
 public class IntegerUtils {
     /**
      * @param n integer value input by client
      * @return the sum of all integers between 0 and not including `n`
      */
     public static Integer getSumOfN(Integer n) {
-        return null;
+        int sum = 0;
+        for(int i = 1; i < n; i++) {
+            sum += i;
+        }
+        return sum;
     }
 
     /**
@@ -14,7 +20,17 @@ public class IntegerUtils {
      * @return integer with identical digits in the reverse order
      */
     public static Integer reverseDigits(Integer val) {
-        return null;
+        String revDig = "";
+        String[] numArr = val.toString().split("");
+
+        for(int i =0; i < numArr.length/2; i++) {
+            String temp = numArr[i];
+            numArr[i] = numArr[numArr.length - i - 1];
+            numArr[numArr.length - i - 1] = temp;
+        }
+
+        Integer strToInt = parseInt(String.join("", numArr));
+        return strToInt;
     }
 
     /**
@@ -22,6 +38,6 @@ public class IntegerUtils {
      * @return true if the integer is even and false if it is not even.
      */
     public static Boolean isEven(Integer val) {
-        return null;
+        return (val % 2 == 0) ? true : false;
     }
 }
